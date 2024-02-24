e1e26be867f84a31c2d964b2f06ee955587dd1b8
zjkang <zk299@nyu.edu>
2024-02-24 13:11:46 -0500

    update add swagger

--- src/main/java/com/alibou/aws/DemoController.java
+++ src/main/java/com/alibou/aws/DemoController.java
@@ -1,17 +1,33 @@
 package com.alibou.aws;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// http://localhost:8080/demo/v1/swagger-ui/index.html
 @RestController
-@RequestMapping("/api/v1/demo")
+@RequestMapping("/getDemo")
 public class DemoController {
 
     private int counter;
     @GetMapping
-    public String demo() {
+    @ApiResponses(
+            @ApiResponse(
+                    responseCode="200",
+                    description="Successful Operation",
+                    content={@Content(
+                            schema=@Schema(implementation = String.class))}
+            )
+    )
+    public ResponseEntity<String> demo() {
         counter++;
-        return String.format("<h1>Response from demo controller: Response number: %d<h1/>", counter);
+        String res = String.format("<h1>Response from demo controller: Response number: %d<h1/>", counter);
+        return ResponseEntity.status(200).body(res);
     }
 }
